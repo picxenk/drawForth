a577d2ff8050535c5f2093fd2ed6c66b0372293e
hyojung seo <seo.hyo@gmail.com>
2019-02-20 15:54:10 +0900

    Update graphics.js

--- javascripts/graphics.js
+++ javascripts/graphics.js
@@ -60,7 +60,7 @@ function Graphics($canvas) {
 
   function triangle(x1, y1, x2, y2, x3, y3) {
     ctx.beginPath();
-    ctx.moveTo(x1, y2);
+    ctx.moveTo(x1, y1);
     ctx.lineTo(x2, y2);
     ctx.lineTo(x3, y3);
     ctx.fill();
